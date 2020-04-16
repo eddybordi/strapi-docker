0428f3a3fceac62ac6ddd3a0698479cb3e339323
Eddy Bordi <eddy@bordi.fr>
2020-04-16 17:46:20 +0200

    init

--- app/plugins/Popover.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue"
-import Popover from 'vue-js-popover'
-
-Vue.use(Popover)
-
-// https://github.com/euvl/vue-js-popover
--- app/plugins/ToggleButton.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue"
-import ToggleButton from 'vue-js-toggle-button'
-
-Vue.use(ToggleButton)
-
-// https://github.com/JonathanDn/vue-toggle-btn
